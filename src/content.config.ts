da783a5687fc456363484e41940294c23c2d9688
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2025-07-13 17:28:30 -0500

    add spacer component. format markdown for TWB

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const worlds = defineCollection({
       publicationDate: z.date(),
       image: image().optional(),
       imageAlt: z.string().optional(),
+      audio: z.string().optional(),
       tags: z.array(z.string()).optional(),
     }),
 });
